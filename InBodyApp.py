190ed2491b08e104e7cd0a64247a55f5337d3c7a
satokazuna <satokazuna@icloud.com>
2023-11-21 00:26:19 +0900

    [Add] 画像読み込み機能

--- InBodyApp.py
+++ InBodyApp.py
@@ -1,1 +1,14 @@
-print("full")
+import cv2
+import numpy as np
+from tkinter import filedialog
+filename = filedialog.askopenfilename()
+print(filename)
+buf = np.fromfile(filename,np.uint8)
+print(type(buf))
+img = cv2.imdecode(buf,cv2.IMREAD_UNCHANGED)
+img = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
+img = cv2.resize(img,(500,200))
+cv2.imshow("Image", img)
+cv2.waitKey()
+
+
